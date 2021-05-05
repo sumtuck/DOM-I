d45fcfa93406b5c794a2aef8b746ea0372470906
sumtuck <31107202+sumtuck@users.noreply.github.com>
2021-05-05 17:28:17 -0600

    NAV and CTA sections complete

--- js/index.js
+++ js/index.js
@@ -40,3 +40,43 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//Start NAV
+
+let navItems = document.querySelectorAll('nav a');
+let nav1 = navItems[0].textContent = (siteContent ["nav"]["nav-item-1"])
+let nav2 = navItems[1].textContent = (siteContent ["nav"]["nav-item-2"])
+let nav3 = navItems[2].textContent = (siteContent ["nav"]["nav-item-3"])
+let nav4 = navItems[3].textContent = (siteContent ["nav"]["nav-item-4"])
+let nav5 = navItems[4].textContent = (siteContent ["nav"]["nav-item-5"])
+let nav6 = navItems[5].textContent = (siteContent ["nav"]["nav-item-6"])
+
+//End NAV 
+
+
+//Start CTA
+
+// 1) the variable "ctaImg" references the HTML document and get the Id "cta-img". 
+// 2) ctaImg.src changes the HTML document to change the image placeholder to the image titled "img-src".
+
+const ctaImg = document.getElementById('cta-img');
+ctaImg.src = siteContent["cta"]["img-src"];
+
+//siteContent referes the "cta" object and the "h1" property within that object, then splits the string "DOM is Awesome" into the substring "DOM, is, Awesome" 
+let ctaHeader = siteContent["cta"]["h1"].split(' ');
+
+// 1) document. is a variable that gives the ability to access the information in the HTML code. 
+// 2)getElementsbyTagName finds "h1" which is the first index in the CTA object. 
+// 3) innerHTML gives the ability to change the value of the identified TagName. 
+// 4) .join rejoins the substrings that were split in step 1 but includes a break between each word in the string because of <br>.
+document.getElementsByTagName('h1')[0].innerHTML = ctaHeader.join('<br>');
+
+// innerText gets the inner text of an object - in this case it gets the inner text of the first index "button" in the "cta" object, 
+
+const button = document.getElementsByTagName('button')[0];
+button.innerText = siteContent ["cta"]["button"];
+
+//End CTA
+
+//Start main-content
+
